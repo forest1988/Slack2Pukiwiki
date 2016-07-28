92bc8f12892f90029056f04dc09cc4eb29ef720b
Yusuke Mori <mori@mi.t.u-tokyo.ac.jp>
2016-07-28 16:31:18 +0900

    Python 2 compatible (unicode_literals)

--- slackAPI.py
+++ slackAPI.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 from slacker import Slacker
 import argparse
 import datetime
